b1783098ad8a86b94f882e3b465f6e7669451734
Arondeke <khmelnitskiy.aleksey@gmail.com>
2023-03-23 22:22:03 +0300

    я ничего не понял(

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+
+        int notPushingWithoutProblems = 5;
         Car car1 = new Car("car1", 4);
         Car car2 = new Car("car2", 4);
 
